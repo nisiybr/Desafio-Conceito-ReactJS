fd95b0d4a36309b92d74d3edc997fc7deea13854
Guilherme Nisiyama <guilherme.nisiyama@gmail.com>
2020-04-10 21:40:03 -0300

    Inclusão de funcionalidades para listar, cadastrar e remover repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories,setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+      const response = await api.post('repositories',{
+      title:`Desafio React JS ${Date.now()}`,
+      url:"https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs",
+      techs:["Typescript","Javascript"]
+    })
+
+    setRepositories([...repositories,response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const data = repositories.filter(repository => repository.id !== id);
+    setRepositories(data);
   }
+  async function loadRepositories() {
+    const response = await api.get('repositories');    
+    setRepositories(response.data);
+  }
+  useEffect(()=>{
+    loadRepositories();
+  },[])
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {
+        repositories.map(repository => (
+          <li key = {repository.id}>
+            {repository.title} 
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))
+      }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
